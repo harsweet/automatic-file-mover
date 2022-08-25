ec4cca36ff1fae4481c4f3c35ab5491c641bfce1
harsweet <hsbhangu0001@gmail.com>
2022-08-24 20:52:06 -0600

    Initialised script, added Welcome Message

---
+++ file_mover_script.py
@@ -0,0 +1,14 @@
+def welcomeMessage():
+    print("\t.... Welcome to the Automatic File Mover ...")
+    print("\n\n\tJust Enter the source location(s), destination location(s) and the file types (.mp3, .mp4, etc)")
+    print("\tAnd the File Mover will take care of the moving :)\n\n\n")
+
+
+
+def main():
+    welcomeMessage()
+
+
+
+if "__name__" == "__main__":
+    main()
